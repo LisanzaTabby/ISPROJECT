6cec6b1ca542cede070db44c9a44a189dfd83229
Tabitha-Lisanza <147367069+LisanzaTabby@users.noreply.github.com>
2024-06-12 11:08:11 +0300

    inital commit

--- smsApp/admin.py
+++ smsApp/admin.py
@@ -5,3 +5,11 @@ class StudentAdmin(admin.ModelAdmin):
     list_display = ('firstname','lastname','email','phone', 'gender','date_added')
 
 admin.site.register(Student,StudentAdmin)
+class TrusteeAdmin(admin.ModelAdmin):
+    list_display = ('firstname','lastname','email','phone', 'gender')
+
+admin.site.register(Trustee,TrusteeAdmin)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('schoolname','Level','Location','phone',)
+
+admin.site.register(School,SchoolAdmin)
--- smsApp/models.py
+++ smsApp/models.py
@@ -16,5 +16,46 @@ class Student(models.Model):
     def __str__(self):
         return f'{self.firstname} {self.lastname}'
     
+class Trustee(models.Model):
+    GENDER = (
+        ('Female', 'Female'),
+        ('Male', 'Male'),
+    )
+    Student = models.ForeignKey(Student, null=True, on_delete=models.SET_NULL)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.IntegerField()
+    gender = models.CharField(max_length=20, choices=GENDER)
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
+class School(models.Model):
+    LEVEL = (
+        ('Primary', 'Primary'),
+        ('Secondary', 'Secondary'),
+        ('Both Primary&Secondary', 'Both Primary&Secondary'),
+        ('Tertiary', 'Tertiary'),
+    )
+    LOCATION = (
+        ('Nairobi', 'Nairobi'),
+        ('Kisumu', 'Kisumu'),
+        ('Mombasa', 'Mombasa'),
+        ('Marsabit', 'Marsabit'),
+        ('Kajiado', 'Kajiado'),
+        ('Lamu', 'Lamu'),
+        ('Kilifi', 'Kilifi'),
+        ('Kakamega', 'Kakamega'),
+        ('Kisii', 'Kisii'),
+        ('Turkana', 'Turkana'),
+    )
+    Student = models.ForeignKey(Student, null=True, on_delete=models.SET_NULL)
+    schoolname = models.CharField(max_length=200)
+    Level = models.CharField(max_length=100, choices=LEVEL)
+    Location = models.CharField(max_length=100,choices=LOCATION)
+    phone = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.schoolname}'
 
 
