ab7743d60a9d44dd8397db3e5ceb86384db32976
Tabitha-Lisanza <147367069+LisanzaTabby@users.noreply.github.com>
2024-06-12 10:20:58 +0300

    initial commit

--- smsApp/urls.py
+++ smsApp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('',views.index,name='index'),
-    path('student',views.student, name='student'),
+    path('admindash',views.admindash, name='admindash'),
+    path('addstudent',views.add_student, name='addstudent'),
+    path('trustee', views.trusteeView, name='trustee'),
+    path('dataentry', views.DataEntryView, name='dataentry'),
+    path('finance', views.FinanceView, name='finance'),
 ]
--- smsApp/views.py
+++ smsApp/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
-
+from .forms import StudentForm
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
-def student(request):
+def admindash(request):
     students = Student.objects.all()
     context= {'students':students}
-    return render(request, 'student.html', context)
+    return render(request, 'admindash.html', context)
+def add_student(request):
+    form = StudentForm()
+    if request.method == 'POST':
+        form =  StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('admindash')
+
+    context = {'form': form}
+    return render(request, 'student_form.html', context)
+def trusteeView(request):
+    context = {}
+    return render(request, 'trustee.html', context)
+def DataEntryView(request):
+    context = {}
+    return render(request, 'dataentry.html', context)
+def FinanceView(request):
+    context = {}
+    return render(request, 'finance.html', context)
